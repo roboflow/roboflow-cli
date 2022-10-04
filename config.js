b32be2f84387a1d944bc071761cdf4f47020913d
Jacobsolawetz <jacobsolawetz@gmail.com>
2022-10-04 13:33:49 -0500

    static config saves

--- config.js
+++ config.js
@@ -6,14 +6,13 @@ let conf;
 
 if (!RF_CLI_ENV) {
     conf = new Conf({
-        projectSuffix: "",
-        projectName: "roboflow",
+        cwd: process.env.HOME + "/.config/roboflow",
         defaults: { app_domain: "app.roboflow.com", api_domain: "api.roboflow.com" }
     });
 } else {
     conf = new Conf({
         projectSuffix: RF_CLI_ENV,
-        projectName: "roboflow",
+        cwd: process.env.HOME + "/.config/roboflow",
         defaults: {
             app_domain: process.env.RF_CLI_APP_DOMAIN,
             api_domain: process.env.RF_CLI_API_DOMAIN
